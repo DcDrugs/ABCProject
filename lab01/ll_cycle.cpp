13e2080318aa8dbceb2bf97d52dc8f82baf2a310
DcDrugs <leshagol@mail.ru>
2021-02-22 03:55:30 +0300

    fifth task added

--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -1,6 +1,20 @@
 #include "ll_cycle.h"
+#define NULL 0
 
 bool ll_has_cycle(node *head) {
-    /* Ваш код должен быть написан только внутри этой функции */
-    return true;
+    node *pnext = head;
+    int i = 0;
+    while(pnext != NULL)
+    {
+    	pnext = pnext->next;
+    	if (pnext == head)
+    		return true;
+    	if(i == 2)
+    	{
+    		i = 0;
+    		head = head->next;
+    	}
+    	i++;
+    }
+    return false;
 }
